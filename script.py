b97a9f38ddf371a626324ccbb8f5643cc9f11162
cmsharma <cmsharma.its@gmail.com>
2021-03-05 18:24:22 +0530

    initial commit

---
+++ script.py
@@ -0,0 +1,38 @@
+#importing libraries
+import os
+import numpy as np
+import flask
+import joblib
+from flask import Flask, render_template, request
+
+#creating instance of the class
+app=Flask(__name__)
+
+#to tell flask what url shoud trigger the function index()
+@app.route('/')
+@app.route('/index')
+def index():
+    return flask.render_template('index.html')
+
+#prediction function
+def ValuePredictor(to_predict_list):
+    to_predict = np.array(to_predict_list).reshape(1,10)
+    loaded_model = joblib.load('model_svc.sav')
+    result = loaded_model.predict(to_predict)
+    return result[0]
+
+
+@app.route('/result',methods = ['POST'])
+def result():
+    if request.method == 'POST':
+        to_predict_list = request.form.to_dict()
+        to_predict_list=list(to_predict_list.values())
+        to_predict_list = list(map(int, to_predict_list))
+        result = ValuePredictor(to_predict_list)
+        
+        if int(result)==1:
+            prediction='There is onset of Heart Stroke!!'
+        else:
+            prediction='System does not predict onset of Stroke!!'
+            
+        return render_template("result.html",prediction=prediction)
